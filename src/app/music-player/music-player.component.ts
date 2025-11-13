b1ca6774f3addeecde1dcf4f8ca0b267b7ee4131
Alyssa Bautista <alyssarhenoanicolebautista@gmail.com>
2025-11-13 15:37:47 +0800

    test

--- src/app/music-player/music-player.component.ts
+++ src/app/music-player/music-player.component.ts
@@ -19,8 +19,8 @@ export class MusicPlayerComponent {
   private readonly animationDuration = 500; // in ms, matches your 0.4s animation
 
   tracks: Track[] = [
-    { title: 'just friends', file: '/music-player/just-friends.ogg' },
-    { title: 'bossa uh', file: '/music-player/bossa-uh.ogg' },
+    { title: 'just friends', file: 'music-player/just-friends.ogg' },
+    { title: 'bossa uh', file: 'music-player/bossa-uh.ogg' },
   ];
 
   currentTrackIndex = 0;
