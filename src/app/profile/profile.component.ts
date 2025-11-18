82b101327b154f6094b69d275275c8cfc0cb02a9
Alyssa Bautista <alyssarhenoanicolebautista@gmail.com>
2025-11-18 14:36:30 +0800

    profile and musin player now on mobile (test)

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -7,5 +7,19 @@ import { Component } from '@angular/core';
   styleUrl: './profile.component.css'
 })
 export class ProfileComponent {
+  protected isActive: boolean = false;
+  protected toggleDisabled = false; // disable toggle during animation
+  private readonly animationDuration = 500; // in ms, matches your 0.4s animation
 
+  //====================================================================================================
+  protected Toggle() {
+    if (this.toggleDisabled) return // ignore clicks if disabled
+
+    this.isActive = !this.isActive
+    this.toggleDisabled = true
+
+    setTimeout(() => {
+      this.toggleDisabled = false
+    }, this.animationDuration)
+  }
 }
