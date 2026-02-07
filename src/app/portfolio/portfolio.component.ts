ec38047fffe5e19aafcc378e20013f7de42ef65a
Alyssa D. Bautista <alyssarhenoanicolebautista@gmail.com>
2026-02-07 18:34:27 +0800

    added video links

--- src/app/portfolio/portfolio.component.ts
+++ src/app/portfolio/portfolio.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Modal } from 'bootstrap';
 
 type PortfolioCategory =
   | 'web'
@@ -10,7 +11,7 @@ interface PortfolioItem {
   title: string;
   category: PortfolioCategory;
   description: string;
-  type: 'image' | 'video';
+  link?: string;
   src: string;
 }
 
@@ -41,31 +42,70 @@ export class PortfolioComponent {
     {
       title: 'United Training Services External Website',
       category: 'web',
+      link: 'https://uts.com.ph/',
       description: 'Redesigned and developed the official UTS website using Blazor.',
-      type: 'image',
       src: 'portfolio/uts-external.png'
     },
     {
       title: 'UTS 2.0 Portal',
       category: 'web',
+      link: 'https://uts2-prod-app-service.azurewebsites.net/',
       description: 'Enterprise Angular SaaS platform focused on asset management.',
-      type: 'image',
       src: 'portfolio/uts2.png'
     },
     {
       title: 'ME-ICT Website',
       category: 'web',
+      link: 'https://me-ict.com/',
       description: 'Modern corporate website designed and built with Angular.',
-      type: 'image',
       src: 'portfolio/meict.png'
     },
     {
       title: 'Data Defenders Website',
       category: 'web',
+      link: 'https://data-defenders.com/',
       description: 'Multi-page corporate website developed using Blazor.',
-      type: 'image',
       src: 'portfolio/dd.png'
-    }
+    },
+
+    //
+
+    {
+      title: 'How to upload certificates through the E-Sea Mobile App',
+      category: 'motion',
+      description: 'https://youtube.com/watch?v=AM1zNu8hqFw',
+      src: 'portfolio/vid-a.jpg'
+    },
+    {
+      title: 'Introducing the E-Sea Mobile App',
+      category: 'motion',
+      description: 'https://youtube.com/watch?v=N5csRAGKc_0',
+      src: 'portfolio/vid-b.jpg'
+    },
+    {
+      title: 'Introduction to UTS Platform',
+      category: 'motion',
+      description: 'https://youtube.com/watch?v=0Lu6E4aNhMI',
+      src: 'portfolio/vid-c.jpg'
+    },
+    {
+      title: 'Training Center Compliance Management Software',
+      category: 'motion',
+      description: 'https://youtube.com/watch?v=TQdPooUjRfU',
+      src: 'portfolio/vid-d.jpg'
+    },
+    {
+      title: 'UTS Promotional Video',
+      category: 'motion',
+      description: 'https://youtube.com/watch?v=Ah7cQc5t_4M',
+      src: 'portfolio/vid-e.jpg'
+    },
+    {
+      title: 'UTS Introduction',
+      category: 'motion',
+      description: 'https://youtube.com/watch?v=pIPOnw-pqz8',
+      src: 'portfolio/vid-f.jpg'
+    },
   ];
 
   setFilter(value: 'all' | PortfolioCategory): void {
@@ -83,4 +123,45 @@ export class PortfolioComponent {
   trackByTitle(index: number, item: PortfolioItem): string {
     return item.title;
   }
+
+
+  //=========================================================
+
+  selectedItem: PortfolioItem | null = null;
+  private modalInstance?: Modal;
+
+  portfolioClick(item: PortfolioItem): void {
+    if (item.category === 'motion') {
+      window.open(item.description, '_blank');
+      return;
+    }
+    if (item.category === 'web') {
+      window.open(item.link, '_blank');
+      return;
+    }
+
+    this.selectedItem = item;
+    this.openModal();
+  }
+
+  openModal(): void {
+    const modalElement = document.getElementById('portfolioModal');
+    if (!modalElement) return;
+
+    this.modalInstance = new Modal(modalElement);
+    this.modalInstance.show();
+  }
+
+  // ========================================================================================================================
+  //Default dialog size
+  protected isModalMaximized: boolean = false;
+
+  protected MaxModal_Toggle(): void {
+    this.isModalMaximized = !this.isModalMaximized;
+  }
+
+  protected Reset(): void {
+    this.isModalMaximized = false;
+  }
+
 }
